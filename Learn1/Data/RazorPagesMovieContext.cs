1f1c618a7c6bed57e81be5a085c84ed4ff934eb4
gao.feng <864691689@qq.com>
2020-06-01 23:03:28 +0800

    first submmit

---
+++ Learn1/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Learn1.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Models.Movie> Movie { get; set; }
+    }
+}
